6bed50932997942ff6fc03f2276a783cd1693a35
Qiwei <qiwei.he@saagie.com>
2022-04-28 15:36:47 +0200

    change init file

--- saagieapi/__init__.py
+++ saagieapi/__init__.py
@@ -2,12 +2,13 @@ import logging
 
 import urllib3
 
+from .pipelines import *
 from .saagie_api import SaagieApi
 
 # Disable urllib3 InsecureRequestsWarnings
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-__all__ = ["SaagieApi"]
+__all__ = ["SaagieApi", "Node", "JobNode", "ConditionNode", "GraphPipeline"]
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s", datefmt="%d/%m/%Y %H:%M:%S")
 logging.getLogger("requests").setLevel(logging.WARN)
