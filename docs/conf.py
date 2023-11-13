17dcc712dc02cdc5f6214e5baa3e9222fd4b2d91
Guillaume Prevost <guillaume.prevost@saagie.com>
2023-11-13 10:48:46 +0100

    Change Sphinx theme to Sphinx-book-theme

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,9 @@ templates_path = ["_templates"]
 
 # -- Options for HTML output
 
-html_theme = "furo"
+html_title = "SaagieAPI"
+html_theme = "sphinx_book_theme"
+html_theme_options = {"show_toc_level": 2, "toc_title": "On this page"}
 
 # -- Options for EPUB output
 epub_show_urls = "footnote"
