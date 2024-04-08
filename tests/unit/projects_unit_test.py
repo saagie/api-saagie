219e715bc641e5c1b9ceb6ba2ffe900fb9bf1042
Guillaume Prevost <guillaume.prevost@saagie.com>
2024-04-08 18:10:00 +0200

    rework project unit tests

--- tests/unit/projects_unit_test.py
+++ tests/unit/projects_unit_test.py
@@ -1,4 +1,6 @@
 # pylint: disable=attribute-defined-outside-init,unused-wildcard-import,protected-access
+from unittest.mock import MagicMock, Mock, patch
+
 import pytest
 from gql import gql
 
@@ -9,12 +11,103 @@ from .saagie_api_unit_test import create_gql_client
 
 
 class TestProjects:
+    @pytest.fixture
+    def saagie_api_mock(self):
+        saagie_api_mock = Mock()
+
+        saagie_api_mock.client.execute = MagicMock()
+        return saagie_api_mock
+
     def setup_method(self):
         self.client = create_gql_client()
 
-    def test_list_projects(self):
+    def test_list_projects_gql(self):
         query = gql(GQL_LIST_PROJECTS)
         self.client.validate(query)
+
+    def test_list_projects(self, saagie_api_mock):
+        project = Projects(saagie_api_mock)
+
+        expected_query = gql(GQL_LIST_PROJECTS)
+
+        project.list()
+
+        saagie_api_mock.client.execute.assert_called_with(query=expected_query, pprint_result=None)
+
+    def test_get_project_id(self, saagie_api_mock):
+        project = Projects(saagie_api_mock)
+
+        project_name = "Project A"
+
+        with patch.object(project, "list") as list_proj:
+            list_proj.return_value = {
+                "projects": [
+                    {
+                        "id": "8321e13c-892a-4481-8552-5be4d6cc5df4",
+                        "name": "Project A",
+                        "creator": "john.doe",
+                        "description": "My project A",
+                        "jobsCount": 49,
+                        "status": "READY",
+                    },
+                    {
+                        "id": "33b70e1b-3111-4376-a839-12d2f93c323b",
+                        "name": "Project B",
+                        "creator": "john.doe",
+                        "description": "My project B",
+                        "jobsCount": 1,
+                        "status": "READY",
+                    },
+                ]
+            }
+            project.get_id(project_name=project_name)
+
+    def test_get_project_id_error(self, saagie_api_mock):
+        project = Projects(saagie_api_mock)
+
+        project_name = "Project C"
+
+        with patch.object(project, "list") as list_proj, pytest.raises(NameError):
+            list_proj.return_value = {
+                "projects": [
+                    {
+                        "id": "8321e13c-892a-4481-8552-5be4d6cc5df4",
+                        "name": "Project A",
+                        "creator": "john.doe",
+                        "description": "My project A",
+                        "jobsCount": 49,
+                        "status": "READY",
+                    },
+                    {
+                        "id": "33b70e1b-3111-4376-a839-12d2f93c323b",
+                        "name": "Project B",
+                        "creator": "john.doe",
+                        "description": "My project B",
+                        "jobsCount": 1,
+                        "status": "READY",
+                    },
+                ]
+            }
+            project.get_id(project_name=project_name)
+
+    def test_get_project_info_gql(self):
+        query = gql(GQL_GET_PROJECT_INFO)
+        self.client.validate(query)
+
+    def test_get_project_info(self, saagie_api_mock):
+        project = Projects(saagie_api_mock)
+
+        project_id = "8321e13c-892a-4481-8552-5be4d6cc5df4"
+
+        params = {"id": project_id}
+
+        expected_query = gql(GQL_GET_PROJECT_INFO)
+
+        project.get_info(project_id=project_id)
+
+        saagie_api_mock.client.execute.assert_called_with(
+            query=expected_query, variable_values=params, pprint_result=None
+        )
 
     def test_get_project_jobs_technologies(self):
         query = gql(GQL_GET_PROJECT_JOBS_TECHNOLOGIES)
@@ -22,10 +115,6 @@ class TestProjects:
 
     def test_get_project_apps_technologies(self):
         query = gql(GQL_GET_PROJECT_APPS_TECHNOLOGIES)
-        self.client.validate(query)
-
-    def test_get_project_info(self):
-        query = gql(GQL_GET_PROJECT_INFO)
         self.client.validate(query)
 
     def test_get_project_info_by_name(self):
