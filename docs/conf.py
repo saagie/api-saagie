ac212806e44da92366f6273a9d144e2174cd70c4
Guillaume Prevost <guillaume.prevost@saagie.com>
2024-05-14 14:36:17 +0200

    WIP

--- docs/conf.py
+++ docs/conf.py
@@ -9,10 +9,10 @@ author = "Saagie Service Team"
 release = "0.1"
 version = "0.1.0"
 
-import os
-import sys
+# import os
+# import sys
 
-sys.path.insert(0, os.path.abspath(".."))
+# sys.path.insert(0, os.path.abspath(".."))
 
 # -- General configuration
 
